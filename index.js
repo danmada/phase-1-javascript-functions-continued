c34e782e13bb5aad51d9c575972dc7054b733f70
danmada <danmada@gmail.com>
2021-06-09 14:26:32 -0500

    commit tree 176a457b56beb0eaf9a184a8ae1b1ab28e664b6d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(todolist="go to the office") {
+
+    return `This Monday, I will ${todolist}.`
+}
+
+function wrapAdjective (highlight="*") {
+    return function encouragement(flair="Special") {
+        return `You are ${highlight}${flair}${highlight}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer")
